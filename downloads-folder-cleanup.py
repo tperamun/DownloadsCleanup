332f6b00f41cf1b17ca7d8b3af147e42da821b80
Timal Peramune <tperamun@uwo.ca>
2020-06-28 16:21:18 -0400

    added extension for video files

--- downloads-folder-cleanup.py
+++ downloads-folder-cleanup.py
@@ -4,15 +4,23 @@ import shutil
 source_folder = "/home/timal/Downloads/"
 files_in_source_folder = os.listdir(source_folder)
 
+
+
+## Paths to the destinations
+
 dest_folder_for_pictures = "/home/timal/Pictures/"
-
-picture_files = ["*.jpeg", "*.png", "*.jpg"]
-
+dest_folder_for_videos = "/home/timal/Videos/"
 
 ### For Pictures ###
 
 for file in files_in_source_folder:
+
+    ###For Pictures ###
     if file.endswith('.png') or file.endswith('.jpeg') or file.endswith('.jpg'):
         shutil.move(os.path.join(source_folder, file), os.path.join(dest_folder_for_pictures, file))
 
+
+    ### For Videos ###
+    if file.endswith('.mov'):
+        shutil.move(os.path.join(source_folder, file), os.path.join(dest_folder_for_videos, file))
 
